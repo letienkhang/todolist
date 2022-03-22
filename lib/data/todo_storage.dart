02968296f95cb98018c8446cb114de2e0b3c3e61
letienkhang <tienkhangle@gmail.com>
2022-03-22 09:45:04 +0700

    implement FileSystemTodoStorage and mock todoStorage

---
+++ lib/data/todo_storage.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+import 'model/todo_model.dart';
+
+
+final TodoStorage todoStorage = FileSystemTodoStorage();
+
+abstract class TodoStorage {
+  Future<List<Todo>> readTodoList();
+}
+
+class FileSystemTodoStorage extends TodoStorage {
+  @override
+  Future<List<Todo>> readTodoList() async {
+    try {
+      final file = await _localFile;
+      final content = await file.readAsString();
+      final List<dynamic> jsonArray = jsonDecode(content);
+      return jsonArray
+          .map(
+            (jsonObject) => Todo.fromJson(jsonObject),
+      )
+          .toList();
+    } catch (e) {
+      return [];
+    }
+  }
+
+  Future<File> get _localFile async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path;
+    return File('$path/todoList.json');
+  }
+}
--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/data/model/todo_model.dart';
-
-final todoList = [
-  Todo(title: 'Do something 1'),
-  Todo(title: 'Do something 2'),
-  Todo(title: 'Do something 3'),
-];
+import 'package:todolist/data/todo_storage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final todoList = todoStorage.readTodoList();
     return Scaffold(
       body: ListView(
         children: todoList
